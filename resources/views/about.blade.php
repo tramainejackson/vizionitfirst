318a35eaaa1696ff4e44e93e85b69390c08d2914
Tramaine Jackson <tjackson@sicom.com>
2021-04-08 15:08:19 -0400

    Removed about us. Left mission statement

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,9 +7,9 @@
 
     <div class="container-fluid">
 
-        <div class="row row-cols-1 row-cols-lg-2" id="">
+        <div class="row row-cols-1" id="">
 
-            <div class="col pt-5" id="" style="padding-bottom: 32%;">
+            <div class="col pt-5 mb-5" id="" style="">
                 <div class="">
                     <!--Content-->
                     <h2 class="mb-3 text-center font-weight-bold display-3">Our Mission</h2>
@@ -17,16 +17,16 @@
                 </div>
             </div>
 
-            <div class="col pt-5 bg-light" id="" style="padding-bottom: 32%;">
-                <div class="">
-                    <!--Content-->
-                    <h2 class="mb-3 text-center font-weight-bold display-3">About Us</h2>
-                    <p style="font-size: 22px;">{{ $settings->about_us !== null ? $settings->about_us : 'No About Us Statement Entered Yet' }}</p>
-                </div>
-            </div>
+            {{--<div class="col pt-5 bg-light" id="" style="padding-bottom: 32%;">--}}
+                {{--<div class="">--}}
+                    {{--<!--Content-->--}}
+                    {{--<h2 class="mb-3 text-center font-weight-bold display-3">About Us</h2>--}}
+                    {{--<p style="font-size: 22px;">{{ $settings->about_us !== null ? $settings->about_us : 'No About Us Statement Entered Yet' }}</p>--}}
+                {{--</div>--}}
+            {{--</div>--}}
 
-            <div class="col position-relative" id="about_us_page_middle_image">
-                <img class="w-100" src="{{ asset('/images/photo13.png') }}" alt="Orientation" />
+            <div class="col text-center mb-5" id="about_us_page_middle_image">
+                <img class="w-75 mx-auto" src="{{ asset('/images/photo13.png') }}" alt="Orientation" />
             </div>
         </div>
     </div>
