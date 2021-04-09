68d047071da815a679d2e143ef4d1f820ba6c064
Tramaine Jackson <tjackson@sicom.com>
2021-04-09 09:21:47 -0400

    Updated jumbotron

--- resources/views/content_parts/jumbotron.blade.php
+++ resources/views/content_parts/jumbotron.blade.php
@@ -1,4 +1,4 @@
-<div class="card card-image page_header z-depth-0" style="background-image: url({{ asset('/images/photo12.png') }}); background-position: 100% 0%;">
+<div class="card card-image page_header z-depth-0" style="background-image: url({{ asset('/images/photo12.png') }}); background-position: 100% 0%; border-radius: 0;">
 
     <div class="text-white text-center rgba-black-strong pb-5 px-4 content-div">
 
